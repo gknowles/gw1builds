de561d80b9b3023d5a7825cd4e1f96262f6bed48
gknowles <gknowles@ieee.org>
2024-09-07 20:27:54 -0700

    More clearly separate old rails code.

--- site/rails/controllers/user_controller.rb
+++ site/rails/controllers/user_controller.rb
@@ -1,9 +1,9 @@
 
 class UserController < ApplicationController
   attr_accessor :trust_root
-  
+
   # Returns json object with :result, :errors, and :user. :errors is an
-  # array of errors, :user is {:name, :role} and :result is 
+  # array of errors, :user is {:name, :role} and :result is
   # one of the following:
   #   bad           - the name and/or password is bad
   #   check_id      - openid must be checked by provider
@@ -19,32 +19,32 @@ class UserController < ApplicationController
     end
     identity_url = normalize_url(identity_url)
 
-    if (UserController.is_dev && identity_url[-5..-1] == '.dev/') 
+    if (UserController.is_dev && identity_url[-5..-1] == '.dev/')
       render_json(:result => :check_id,
         :checkid_url => "#{ url_for(:action => 'complete') }" +
           "?open_id_dev=1&openid_url=#{URI.escape(identity_url)}"
       )
       return
     end
-    
+
     oi_request = timeout_protection_from_identity_server {
-      open_id_consumer.begin(identity_url) 
+      open_id_consumer.begin(identity_url)
     }
     case oi_request.status
     when OpenID::SUCCESS
-      add_simple_registration_fields(oi_request, 
+      add_simple_registration_fields(oi_request,
         :optional => [ :nickname ])
       trust_root = TRUST_ROOT || url_for(:controller => '/')
       return_to = url_for(:action => 'complete')
       checkid_url = oi_request.redirect_url(trust_root, return_to, true)
-      session[:checkid_setup] = 
+      session[:checkid_setup] =
         oi_request.redirect_url(trust_root, return_to)
       @json = { :result => :check_id, :checkid_url => checkid_url }
     when OpenID::FAILURE
       @json = { :result => :bad, :errors => [
         oi_request.msg || "OpenID server not found"] }
     else
-      @json = { :result => :bad, :errors => ["Unknown error"] }    
+      @json = { :result => :bad, :errors => ["Unknown error"] }
     end
     @json[:user] = { :identity_url => identity_url, :role => User::GUEST }
     render_json
@@ -63,14 +63,14 @@ class UserController < ApplicationController
       status = OpenID::SUCCESS
     else
       consumer = open_id_consumer
-      oi_response = timeout_protection_from_identity_server { 
-        consumer.complete(params) 
+      oi_response = timeout_protection_from_identity_server {
+        consumer.complete(params)
       }
       identity_url = oi_response.identity_url
       identity_url = normalize_url(identity_url) if identity_url
       status = oi_response.status
     end
-    
+
     case status
     when OpenID::SUCCESS
       session[:checkid_setup] = nil
@@ -82,22 +82,22 @@ class UserController < ApplicationController
         end
         session[:identity_url] = identity_url
         @json = { :result => :signup_needed,
-          :user => { :name => nickname, :identity_url => identity_url, 
+          :user => { :name => nickname, :identity_url => identity_url,
             :role => User::GUEST }
           }
       else
-        user.login_at = ActiveRecord::Base.default_timezone == :utc ? 
+        user.login_at = ActiveRecord::Base.default_timezone == :utc ?
           Time.now.utc : Time.now
         user.save
-        @json = { :result => :ok, :user => { 
+        @json = { :result => :ok, :user => {
           :id => user.id, :name => user.name, :role => user.role } }
         # For some reason this 'current_user = user' isn't reflected inside
         # the ApplicationController::* methods
-        session[:user] = user 
+        session[:user] = user
         get_group_list @json, true
       end
     when OpenID::SETUP_NEEDED
-      @json = { :result => :setup_needed, 
+      @json = { :result => :setup_needed,
         :user => { :identity_url => identity_url, :role => User::GUEST },
         :setup_url => session[:checkid_setup] } # oi_response.setup_url }
     when OpenID::CANCEL
@@ -105,16 +105,16 @@ class UserController < ApplicationController
     when OpenID::FAILURE
       @json = { :result => :bad, :errors => [
         oi_response.msg || "Verification failed"] }
-    else      
-      @json = { :result => :bad, 
+    else
+      @json = { :result => :bad,
         :errors => ["Unknown response from OpenID server"] }
     end
   end # complete
 
-  
+
   def signup
     return unless
-      ensure_method(:post, :xhr) and 
+      ensure_method(:post, :xhr) and
       ensure_guest and
       ensure_presence_of('user.name')
 
@@ -124,17 +124,17 @@ class UserController < ApplicationController
         request.request_uri] )
       return
     end
-    
+
     user = User.new
     user.attributes = params[:user]
     user.identity_url = session[:identity_url]
-    user.login_at = ActiveRecord::Base.default_timezone == :utc ? 
+    user.login_at = ActiveRecord::Base.default_timezone == :utc ?
       Time.now.utc : Time.now
-    
+
     # save wrapped in transaction with email, so that failure to send
     # rolls back the user
     if user.save
-      @json = { :result => :ok, :user => { 
+      @json = { :result => :ok, :user => {
         :id => user.id, :name => user.name, :role => user.role } }
       session[:user] = user;
       get_group_list @json, true
@@ -154,7 +154,7 @@ class UserController < ApplicationController
     if user.nil?
       render_json(:result => :ok, :user => { :role => User::GUEST })
     else
-      @json = { :result => :ok, 
+      @json = { :result => :ok,
         :user => { :id => user.id, :name => user.name, :role => user.role }
         }
       get_group_list @json, true
@@ -167,5 +167,5 @@ class UserController < ApplicationController
     reset_session
     render_json(:result => :ok, :user => { :role => User::GUEST })
   end # logout
-  
+
 end
